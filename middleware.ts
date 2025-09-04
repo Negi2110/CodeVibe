e9d2b152f7b02f6b4dfe880bba97e93f174c9bc4
Aman Negi <negi.aman5000@gmail.com>
2025-09-04 13:46:04 +0530

    added sign-in and  auth

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,44 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 import {
-     DEFAULT_LOGIN_REDIRECT,
-     apiAuthPrefix,
-     publicRoutes,
-     authRoutes
-}from "@/routes"
-
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from "@/routes";
 import authConfig from "./auth.config";
 
-const {auth}=NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
 
-export default auth((req)=>{
-    const {nextUrl}=req
-    const isLoggedIn= !!req.auth
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
-     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute) {
+  if (isApiAuthRoute) {
     return null;
-    }
+  }
 
-    if (isAuthRoute) {
+  if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return null;
-    }
-    if(!isLoggedIn && !isPublicRoute){
-    return Response.redirect(new URL("/auth/sign-in" , nextUrl))
-    }
+  }
 
-    return null
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/sign-in", nextUrl));
+  }
+
+  return null;
 });
 
 export const config = {
+  // copied from clerk
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
