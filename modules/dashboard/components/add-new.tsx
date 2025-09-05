b13e6e71f471de33dc2b15ff9e7e0674df7223a6
Aman Negi <negi.aman5000@gmail.com>
2025-09-05 17:48:01 +0530

    Playground Funtionality

--- modules/dashboard/components/add-new.tsx
+++ modules/dashboard/components/add-new.tsx
@@ -2,16 +2,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
-// import { createPlayground } from "@/features/playground/actions";
 import { Plus } from 'lucide-react'
 import Image from "next/image"
 import { useRouter } from "next/navigation";
 import { useState } from "react"
 import { toast } from "sonner";
 import TemplateSelectingModal from "./template-selecting-modal";
+import { createPlayground } from "../actions";
 
 const AddNewButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+   const [selectedTemplate, setSelectedTemplate] = useState<{
+    title: string;
+    template: "REACT" | "NEXTJS" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR";
+    description?: string;
+  } | null>(null)
+  const router = useRouter()
+
+  const handleSubmit = async (data:{
+      title: string;
+    template: "REACT" | "NEXTJS" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR";
+    description?: string;
+  })=>{
+    setSelectedTemplate(data)
+
+    const res = await createPlayground(data);
+    toast.success("Playground Created successfully")
+    setIsModalOpen(false)
+    router.push(`/playground/${res?.id}`)
+  }
 
   return (
     <>
@@ -50,7 +69,7 @@ const AddNewButton = () => {
       <TemplateSelectingModal
        isOpen={isModalOpen}
        onClose={()=>setIsModalOpen(false)}
-       onSubmit={()=>{}}
+       onSubmit={handleSubmit}
       />
       
     </>
